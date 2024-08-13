f6afe14bb5c9c1ae87edb8d3377ac40a83c6a926
seulki <cjdcjs2230@naver.com>
2024-08-13 12:21:05 +0900

    2024/08/13 12:21

--- server/db/database_mysql80.js
+++ server/db/database_mysql80.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "127.0.0.1", // host : "서버주소(ip)" . 실무에서는 cmd창에 ipconfig 입력해서 IPv4에 나온 주소 쓰기
   port: "3306", // 워크벤치 메인에서 확인가능
   user: "root",
-  password: "tmfrl119!!", // MySQL 비번설정했던거
+  password: "tmfrl119!!",
   database: "jeju_beer",
 });
 
